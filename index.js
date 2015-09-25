681fa6eee366194392be1f42d94b5f6a15050231
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-09-25 14:41:05 -0700

    Refactor files

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 let express = require('express')
 let requireDir = require('require-dir')
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
-
 let App = require('./app/app')
 let config = requireDir('./config', {recurse: true})
 
-let app = new App(config)
 let port = process.env.PORT || 8000
+let app = new App(config)
 
 app.initialize(port)
 .then(() => console.log(`Listening at http://127.0.0.1:${port}`))
