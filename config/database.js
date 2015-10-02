8d883505f8d73094597de5c4a81bec77a71b186c
Gauri Shankar P <gauri.gshankar@yahoo.co.in>
2015-10-01 18:00:20 -0700

    Adding Mongo Heroku instance

--- config/database.js
+++ config/database.js
@@ -2,5 +2,9 @@ module.exports = {
 	'development': {
 		'type': 'mongo',
 		'url': 'mongodb://127.0.0.1:27017/lets-watch'
+	},
+	'production' :{
+		'type' : 'mongo',
+		'url': process.env['MONGOLAB_URI']
 	}
 }
