7aab8683d0bdf4244b95a620c6fe0b0c213c881c
Gauri Shankar P <gauri.gshankar@yahoo.co.in>
2015-10-01 17:52:33 -0700

    Heroku Deploy Fixes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-require('babel/polyfill');
-let express = require('express')
-let requireDir = require('require-dir')
+require('babel/register');
+var requireDir = require('require-dir')
 
-let App = require('./app/app')
-let config = requireDir('./config', {recurse: true})
+var App = require('./app/app')
+var config = requireDir('./config', {recurse: true})
 
-let port = process.env.PORT || 8000
-let app = new App(config)
+var port = process.env.PORT || 8000
+var app = new App(config)
 
 app.initialize(port)
-.then(() => console.log(`Listening at http://127.0.0.1:${port}`))
-.catch(e => console.log(e.stack ? e.stack : e))
+    .then(function () {
+        console.log("Listening at http://127.0.0.1:"+port)
+    })
+        .catch(function (e){
+            console.log(e.stack ? e.stack : e)
+        })
